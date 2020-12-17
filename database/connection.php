296ed20655b73d43ae43f266dc1c77156bc4cdf9
matheus-nicolay <58345766+matheus-nicolay@users.noreply.github.com>
2020-12-16 22:35:54 -0300

    Add login, register, update

---
+++ database/connection.php
@@ -0,0 +1,3 @@
+<?php
+  $dbc = mysqli_connect('localhost','root','','alfabarber') or die ('Erro ao conectar ao banco de dados');
+?>
---
+++ src/code/register/registerUser.php
@@ -0,0 +1,31 @@
+<?php
+  include '../../../database/connection.php';
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+    $query_select = "SELECT email FROM userb WHERE email = '$email'";
+    $result_select = mysqli_query($dbc, $query_select) or die ('Erro ao executar o comando SQL');
+    $array = mysqli_fetch_array($result_select);
+    $logarray = $array['email'];
+
+    if($email == "" || $email == null){
+        echo "<script language='javascript' type='text/javascript'>alert('O campo email deve ser preenchido');window.location.href='../../../pages/register/index.html';</script>";
+    }else{
+        if($logarray == $email){
+            echo "<script language='javascript' type='text/javascript'>alert('Este email já está registrado em outra conta!');window.location.href='../../../pages/register/index.html';</script>";
+            die();
+        }else{
+            $query_insert = "INSERT INTO userb (name, email, password, type) VALUES ('$name', '$email', SHA('$password'),0)";
+            $insert = mysqli_query($dbc, $query_insert) or die ('Erro ao executar o comando SQL');
+
+            if($insert){
+                echo "<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='../../../index.html';</script>";
+            }else{
+                echo "<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar o usuário');window.location.href='../../../pages/register/index.html';</script>";
+            }
+        }
+    }
+
+?>
